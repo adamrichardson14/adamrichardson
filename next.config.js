3125cdb57a14b28fa0d33151ef2a440e919a55cd
adamrichardson14 <45330519+adamrichardson14@users.noreply.github.com>
2021-03-29 16:55:06 +0100

    generate sitemap

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ["images.unsplash.com", "upload.wikimedia.org", "i.ytimg.com"],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./lib/siteMap");
+    }
+
+    return config;
+  },
 };
